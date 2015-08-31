f9d2ca370af163efd30b3e0b9b6e14c82bded856
Derek Thurn <derek@fake.email>
2015-08-31 15:06:47 -0700

    animator controllers

--- Steelmage/Assets/Scripts/Gladiator.cs
+++ Steelmage/Assets/Scripts/Gladiator.cs
@@ -4,21 +4,24 @@ public class Gladiator : MonoBehaviour {
   public Transform GunPosition;
 
   private bool _isFiring;
+  private Animator _animator;
+  private readonly int _shootRifleHash = Animator.StringToHash("shootRifle");
 
   // Use this for initialization
   void Start() {
-
+    _animator = GetComponent<Animator>();
   }
 
   // Update is called once per frame
   void Update() {
     if (!_isFiring && Input.GetKeyDown(KeyCode.Mouse0)) {
-      F3DController.Instance.Fire(F3DEffectType.Vulcan, GunPosition);
+      //F3DController.Instance.Fire(F3DEffectType.Vulcan, GunPosition);
       _isFiring = true;
+      _animator.SetTrigger(_shootRifleHash);
     }
 
     if (_isFiring && Input.GetKeyUp(KeyCode.Mouse0)) {
-      F3DController.Instance.Stop();
+      //F3DController.Instance.Stop();
       _isFiring = false;
     }
   }
