674d24f0aebc7f1503e8b11b4585b085de44b198
Derek Thurn <derek@fake.email>
2015-09-06 15:08:10 -0700

    converting from animator to legacy

--- Steelmage/Assets/Scripts/Gladiator.cs
+++ Steelmage/Assets/Scripts/Gladiator.cs
@@ -3,26 +3,38 @@
 public class Gladiator : MonoBehaviour {
   public Transform GunPosition;
 
-  private Animator _animator;
+  private Animation _animation;
   private bool _walking;
 
-// Use this for initialization
   void Start() {
-    _animator = GetComponent<Animator>();
+    _animation = GetComponent<Animation>();
+    _animation.Play("guns_idle");
   }
 
   public void FireRifle() {
     F3DController.Instance.Fire(F3DEffectType.Vulcan, GunPosition);
   }
 
+  public void WalkLoopPoint() {
+    Debug.Log("Walk Loop Point " + _walking);
+    if (_walking) {
+      _animation.Play("guns_walk_loop", PlayMode.StopAll);
+    }
+    else {
+      _animation.Play("guns_idle");      
+    }
+  }
+
   void Update() {
     if (Input.GetKeyDown(KeyCode.Mouse0)) {
-      _animator.SetTrigger("shootRifle");
+      _animation.Play("shot_rifle", PlayMode.StopAll);
     }
 
     if (Input.GetKeyDown(KeyCode.Space)) {
       _walking = !_walking;
-      _animator.SetBool("walking", _walking);
+      if (_walking) {
+        _animation.Play("guns_walk", PlayMode.StopAll);
+      }
     }
   }
 }
