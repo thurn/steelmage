5d36f6c47011c4c2106a802011954a6784fea3f0
Derek Thurn <derek@fake.email>
2015-10-02 08:43:01 -0700

    normalize line endings

--- Steelmage/Assets/Scripts/Gladiator.cs
+++ Steelmage/Assets/Scripts/Gladiator.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+using UnityEngine;
 
 enum GladiatorState {
   Walking,
--- Steelmage/Assets/Scripts/HelloWorld.cs
+++ Steelmage/Assets/Scripts/HelloWorld.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+using UnityEngine;
 
 public class HelloWorld : MonoBehaviour {
   void Start() {
