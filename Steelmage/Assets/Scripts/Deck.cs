80980ce286ce15029aa1c9afcb43584c7163bda2
Derek Thurn <derek@fake.email>
2015-10-10 09:35:03 -0700

    flip on y axis

---
+++ Steelmage/Assets/Scripts/Deck.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Steelmage {
+  public class Deck : MonoBehaviour {
+    private Animation _animation;
+
+    public void Start() {
+      _animation = GetComponent<Animation>();
+    }
+
+    public void DrawCard() {
+      Debug.Log("Draw Card");
+      _animation.Play("drawCard");
+    }
+  }
+}
