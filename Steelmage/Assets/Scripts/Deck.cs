3bdbd5368f8ee16760353d78b6403222c72ca243
Derek Thurn <derek@fake.email>
2015-10-14 09:18:22 -0700

    flipping shows new front

--- Steelmage/Assets/Scripts/Deck.cs
+++ Steelmage/Assets/Scripts/Deck.cs
@@ -1,11 +1,20 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 namespace Steelmage {
   public class Deck : MonoBehaviour {
+    public Sprite CardFront;
+
     private Animation _animation;
+    private Image _image;
 
     public void Start() {
       _animation = GetComponent<Animation>();
+      _image = GetComponent<Image>();
+    }
+
+    public void Flip() {
+      _image.sprite = CardFront;
     }
 
     public void DrawCard() {
