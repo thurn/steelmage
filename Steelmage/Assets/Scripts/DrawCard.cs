3be72a6c64e76d0743818a7831b760f258e5ab09
Derek Thurn <derek@fake.email>
2015-10-22 22:03:18 -0700

    flip tween

--- Steelmage/Assets/Scripts/DrawCard.cs
+++ Steelmage/Assets/Scripts/DrawCard.cs
@@ -4,11 +4,12 @@ using UnityEngine;
 namespace Steelmage {
   public class DrawCard : MonoBehaviour {
     private static DrawCard _instance;
+    public Card CardPrefab;
+    public Transform ShowCardPosition;
+
     public static DrawCard Instance {
       get { return _instance ?? (_instance = FindObjectOfType<DrawCard>()); }
     }
-
-    public Card CardPrefab;
 
     public void Draw(Vector3 deckPosition) {
       var card = Instantiate(CardPrefab);
@@ -18,7 +19,11 @@ namespace Steelmage {
       card.transform.SetAsLastSibling();
       DOTween.Sequence()
         .Append(card.transform.DOMove(new Vector2(0, 100), 0.5f).SetRelative())
-        .Append(card.transform.DOMove(transform.position, 3.0f));
+        .Append(DOTween.Sequence()
+          .Append(card.transform.DOMove(ShowCardPosition.position, 1.0f))
+          .Insert(0, card.transform.DOScale(new Vector3(3, 3, 1), 1.0f))
+          .Insert(0, card.transform.DORotate(new Vector3(0, 90, 0), 0.5f))
+          .Insert(0.5f, card.transform.DORotate(Vector3.zero, 0.5f)));
     }
   }
 }
