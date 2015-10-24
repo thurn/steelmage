45a7081b0a96f2bc0de79faa8bb4d440a66597c1
Derek Thurn <derek@fake.email>
2015-10-24 11:25:24 -0700

    hand moving animation

--- Steelmage/Assets/Scripts/Canvas.cs
+++ Steelmage/Assets/Scripts/Canvas.cs
@@ -10,8 +10,8 @@ namespace Steelmage {
 
     public T InstantiateObject<T>(Object newObject, Vector3 position) where T : class {
       var value = (MonoBehaviour)Instantiate(newObject, position, Quaternion.identity);
-      value.transform.SetParent(transform, false /* worldPositionStays */);
-      value.transform.SetAsFirstSibling();
+      value.transform.SetParent(transform, true /* worldPositionStays */);
+      value.transform.localScale = Vector3.one;
       return value as T;
     }
   }
