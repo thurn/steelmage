85dba6b1508d6b4f552deb9deef5bfd9f36db766
Derek Thurn <derek@fake.email>
2015-10-26 11:32:25 -0700

    vectrosity

---
+++ Steelmage/Assets/Scripts/Grid.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using Vectrosity;
+
+public class Grid : MonoBehaviour {
+  // Use this for initialization
+  private void Start() {
+    VectorLine.SetLine(Color.green, new Vector2(0, 0), new Vector2(Screen.width - 1, Screen.height - 1));
+  }
+
+  // Update is called once per frame
+  private void Update() {}
+}
