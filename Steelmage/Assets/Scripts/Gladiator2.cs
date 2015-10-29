a1c077667f43a912059ffc750881b53c21a09f05
Derek Thurn <derek@fake.email>
2015-10-29 11:33:33 -0700

    gladiator2 script

---
+++ Steelmage/Assets/Scripts/Gladiator2.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gladiator2 : MonoBehaviour {
+  private Animator _animator;
+
+  void Start() {
+    _animator = GetComponent<Animator>();
+  }
+
+  void Update() {
+    if (Input.GetKeyDown(KeyCode.V)) {
+      _animator.SetTrigger("Walk");
+    }
+  }
+}
