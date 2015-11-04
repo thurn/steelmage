e7eca30f1c42f70d3e563f6327305ffc60491592
Derek Thurn <derek@fake.email>
2015-11-03 20:56:46 -0800

    clean up legacy animation

--- Steelmage/Assets/Scripts/Gladiator2.cs
+++ Steelmage/Assets/Scripts/Gladiator2.cs
@@ -2,19 +2,12 @@
 
 public class Gladiator2 : MonoBehaviour {
   private Animator _animator;
-  private Animation _animation;
 
   public void Start() {
     _animator = GetComponent<Animator>();
-    _animation = GetComponent<Animation>();
   }
 
   public void Update() {
-    if (!_animation.isPlaying) {
-      _animation.enabled = false;
-      _animator.enabled = true;
-    }
-
     if (Input.GetKeyDown(KeyCode.V)) {
       _animator.SetTrigger("Walk");
     } else if (Input.GetKeyDown(KeyCode.B)) {
@@ -23,10 +16,8 @@ public class Gladiator2 : MonoBehaviour {
       _animator.SetTrigger("Draw");
     } else if (Input.GetKeyDown(KeyCode.L)) {
       _animator.SetTrigger("Idle");
-    } else if (Input.GetKeyDown(KeyCode.M) && !_animation.isPlaying) {
-      _animator.enabled = false;
-      _animation.enabled = true;
-      _animation.Play("shot_rifle");
+    } else if (Input.GetKeyDown(KeyCode.M)) {
+      _animator.SetTrigger("Shoot");
     }
   }
 
