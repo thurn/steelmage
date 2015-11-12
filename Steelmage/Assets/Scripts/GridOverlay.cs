d249025fdf5665479e872e322bfe102480d03974
Derek Thurn <derek@fake.email>
2015-11-12 09:02:57 -0800

    further grid overlay

--- Steelmage/Assets/Scripts/GridOverlay.cs
+++ Steelmage/Assets/Scripts/GridOverlay.cs
@@ -15,9 +15,6 @@ namespace Steelmage {
           var pointArray = new[] {cell.MinPoint3D, cell.CenterPoint3D};
           var points = new VectorLine("Points", pointArray, null, 2.0f) {color = Color.green};
           points.Draw3DAuto();
-          var pointArray2 = new[] {cell.CenterPoint3D, cell.MaxPoint3D};
-          var points2 = new VectorLine("Points2", pointArray2, null, 2.0f) {color = Color.red};
-          points2.Draw3DAuto();
         }
       }
     }
