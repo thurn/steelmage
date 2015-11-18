f45c3073035626f58244f6ff100bdde91d23985c
Derek Thurn <derek@fake.email>
2015-11-18 08:35:31 -0800

    identifying hovered cell

---
+++ Steelmage/Assets/Scripts/CellSelector.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class CellSelector : MonoBehaviour {
+  private GGCell _hoverCell;
+
+  private void Update() {
+    var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+    var cell = GGGrid.GetCellFromRay(ray, 1000f);
+    if (cell != _hoverCell) {
+      if (cell != null) {
+        Debug.Log("Hovering Cell: " + cell.GridX + "," + cell.GridY);
+      }
+      _hoverCell = cell;
+    }
+  }
+}
