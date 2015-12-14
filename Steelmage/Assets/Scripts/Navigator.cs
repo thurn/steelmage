05a4057dc9cfe32001163b8c99077fb3ba6bdfd1
Derek Thurn <thurn@users.noreply.github.com>
2015-12-13 21:44:13 -0500

    navigator

---
+++ Steelmage/Assets/Scripts/Navigator.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Navigator : MonoBehaviour {
+  public Vector3 MoveDirection = Vector3.zero;
+  public float MoveMagnitude = 0.0f;
+  public float HorizontalMoveMagnitude = 0.0f;
+  public float VerticalMoveMagnitude = 0.0f;
+
+  private void Start() {}
+
+  private void Update() {}
+}
