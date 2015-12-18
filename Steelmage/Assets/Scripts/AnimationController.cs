3199da44768fdd8057e8a3b761321c4cd626a34e
Derek Thurn <thurn@users.noreply.github.com>
2015-12-17 21:44:50 -0500

    more animation controller

--- Steelmage/Assets/Scripts/AnimationController.cs
+++ Steelmage/Assets/Scripts/AnimationController.cs
@@ -23,16 +23,19 @@ namespace Steelmage {
     // Update is called once per frame
     private void Update() {
       if (Input.GetKeyDown(KeyCode.N)) {
-        var relativePosition = Target.position - transform.position;
-        var rotation = Quaternion.LookRotation(relativePosition, Vector3.up);
-        var walkStartAngle = NormalizeAngle(rotation.eulerAngles.y);
-        Debug.Log("walkStartAngle " + walkStartAngle);
-        _animator.SetFloat("WalkStartAngle", walkStartAngle);
         _animator.SetFloat("InputMagnitude", 0.5f);
+
       }
+      var relativePosition = Target.position - transform.position;
+      var targetRotation = Quaternion.LookRotation(relativePosition, Vector3.up);
+      var deltaRotation = Quaternion.FromToRotation(transform.forward, relativePosition);
+      var walkStartAngle = NormalizeAngle(deltaRotation.eulerAngles.y);
+      Debug.Log("walkStartAngle " + walkStartAngle);
+      _animator.SetFloat("WalkStartAngle", walkStartAngle);
     }
 
     private static float NormalizeAngle(float angle) {
+      if (Mathf.Abs(angle) < 5) return 0;
       if (Mathf.Abs(angle - 360) < Mathf.Abs(angle)) return angle - 360;
       return angle;
     }
