b49a483931b6d60997cc66168f2bcfb0055d8b28
Derek Thurn <thurn@users.noreply.github.com>
2016-01-26 19:28:43 -0800

    tuning navigation

--- Steelmage/Assets/Scripts/AnimationController.cs
+++ Steelmage/Assets/Scripts/AnimationController.cs
@@ -17,30 +17,41 @@ namespace Steelmage {
     public Transform Target2;
     private int _inputAngleResetCounter;
     private bool _target2;
+    private NavMeshAgent _navMeshAgent;
+    private bool _navigate;
 
     private void Start() {
       _animator = GetComponent<Animator>();
       _animationState = AnimationState.Standing;
+      _navMeshAgent = GetComponent<NavMeshAgent>();
+      _navMeshAgent.updatePosition = false;
     }
 
     // Update is called once per frame
     private void Update() {
       if (Input.GetKeyDown(KeyCode.N)) {
-        _animator.SetFloat("WalkStartAngle", AngleToTarget(transform, Target));
+        _animator.SetFloat("WalkStartAngle", AngleToTarget(transform, Target.position));
         _animator.SetFloat("InputMagnitude", 0.5f);
       }
 
       if (Input.GetKeyDown(KeyCode.M)) {
-        _target2 = true;
+        //_target2 = true;
+        _navigate = true;
+        _navMeshAgent.SetDestination(Target2.position);
       }
 
-      if (_target2 && Vector3.Distance(transform.position, Target2.position) > 5.0f) {
-        _animator.SetFloat("InputAngle", AngleToTarget(transform, Target2));
+      //if (_target2 && Vector3.Distance(transform.position, Target2.position) > 5.0f) {
+        //_animator.SetFloat("InputAngle", AngleToTarget(transform, Target2.position));
+      //}
+
+      if (_navigate) {
+        Debug.Log(_navMeshAgent.nextPosition + "<<>>" + AngleToTarget(transform, _navMeshAgent.nextPosition));
+        _animator.SetFloat("InputAngle", AngleToTarget(transform, _navMeshAgent.nextPosition));
       }
     }
 
-    private static float AngleToTarget(Transform source, Transform target) {
-      var relativePosition = target.position - source.position;
+    private static float AngleToTarget(Transform source, Vector3 targetPosition) {
+      var relativePosition = targetPosition - source.position;
       var lookRotation = Quaternion.LookRotation(relativePosition, Vector3.up);
       var walkStartAngle = NormalizeAngle(lookRotation.eulerAngles.y);
       var currentRotation = NormalizeAngle(source.rotation.eulerAngles.y);
